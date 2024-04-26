8adf5e8f58df3606feefe99ea56a0a862476ba66
Rakim Ali <rakimali770@gmail.com>
2024-04-26 20:08:54 +0600

    learning object

--- object.js
+++ object.js
@@ -3,6 +3,20 @@ let obj = {
     model : "S24 ultra",
     camera: ["12MP","12MP","12MP Telephoto"],
     processor: "snapdragon 435",
-    price : "1300$"
+    price : "1300$",
+    "what the price": 12,
+    "its bollean value":true,
 }
-console.log(obj.brand);
+
+console.log(obj["what the price"]);
+console.log(obj["its bollean value"]);
+
+
+// how to use symbolol in javascript
+
+let newSmby=Symbol("rakim")
+let key={
+    [newSmby]: "hello rakim"
+}
+
+console.log(key[newSmby]);
