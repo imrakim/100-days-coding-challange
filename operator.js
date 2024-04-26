ad303a923ae976715a6b5e4928356ac18c0e7d9f
Rakim Ali <rakimali770@gmail.com>
2024-04-26 10:15:21 +0600

    starting of operator

---
+++ operator.js
@@ -0,0 +1,39 @@
+// aritchmetich operator
+
+//addition operator
+
+let a = 4;
+let b = 4;
+let ab = a+b; //ans:8
+console.log(ab);
+
+//subtriction operator
+let c = 5;
+let d = 3;
+let cd = c-d; // ans:2
+console.log(cd);
+
+//multipliction operator
+
+let e = 5
+let f = 5;
+let ef = e*f; //ans:25
+console.log(ef);
+
+
+//divide operator
+let g = 10
+let h = 5;
+let gh= g/h; //ans:2
+console.log(gh);
+
+// modulas operator
+let i = 50;
+let j = 40;
+let ij= i%j; //ans:10
+console.log(ij);
+
+
+
+
+
