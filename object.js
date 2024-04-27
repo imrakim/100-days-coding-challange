afaf985698d69c7f86cf8512b5dea760fea24dec
Rakim Ali <rakimali770@gmail.com>
2024-04-27 11:53:37 +0600

    learninig how to shaw javascript object value

--- object.js
+++ object.js
@@ -1,28 +1,27 @@
+let newSmby = Symbol("rakim");
 let obj = {
-    brand : "samsung",
-    model : "S24 ultra",
-    camera: ["12MP","12MP","12MP Telephoto"],
-    processor: "snapdragon 435",
-    price : "1300$",
-    "what the price": 12,
-    "its bollean value":true,
-}
+  brand: "samsung",
+  model: "S24 ultra",
+  camera: ["12MP", "12MP", "12MP Telephoto"],
+  processor: "snapdragon 435",
+  price: "1300$",
+  "what the price": 12,
+  "its bollean value": true,
+  [newSmby]:"hlw rakim"//use symbol in javascript
+};
 
-obj.model="S25 Ultra"
+obj.model = "S25 Ultra";
 console.log(obj.model); //change the object key value
 console.log(obj["what the price"]);
 console.log(obj["its bollean value"]);
 // Object.freeze(obj); // you will do not change anything
 // obj.brand="xiaomi"
 // console.log(obj.brand);
-console.log(obj.hasOwnProperty("camera"));
+console.log(obj.hasOwnProperty("camera")); 
+console.log(obj[newSmby]);
+console.log(Object.keys(obj)); //show all keys name
+console.log(Object.values(obj));//show all key values
 
 
-// how to use symbolol in javascript
 
-let newSmby=Symbol("rakim")
-let key={
-    [newSmby]: "hello rakim"
-}
 
-console.log(key[newSmby]);
