1aa9fa5a46015aaadee80dc7065e778ddc2c353f
Rakim Ali <rakimali770@gmail.com>
2024-04-28 12:57:54 +0600

    statement if

--- condition.js
+++ condition.js
@@ -1,10 +1,21 @@
 //let check simple condition statements
 
-
 let age = 14;
-if(age>18){
-    console.log('you can vote');
+if (age > 18) {
+  console.log("you can vote");
 }
-if(age<18){
-    console.log('you can not vote');
+if (age < 18) {
+  console.log("you can not vote");
 }
+
+let mode = "dark";
+let color;
+
+if (mode === "dark") {
+  color ="black";
+}
+if (mode === "light") {
+  color ="white";
+}
+
+console.log(color);
