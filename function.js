4847f479a4a0265a4da31d8325dab5c0b290fd53
Rakim Ali <rakimali770@gmail.com>
2024-04-28 11:20:49 +0600

    learning js function

---
+++ function.js
@@ -0,0 +1,14 @@
+// function myFunction(fullName){
+//     console.log('hello' + fullName);
+// }
+// myFunction(' rakim')
+// myFunction(' bhai')
+// myFunction(' web Designer')
+
+
+function helloJs(fristName,lastName,age,dateofbirth=1991,hello){
+
+console.log(fristName + lastName);
+
+}
+helloJs('Rakim',' Ali')
