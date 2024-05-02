77079ac161374cce941009edf7931cf52c002c69
Rakim Ali <rakimali770@gmail.com>
2024-05-02 12:52:38 +0600

    making html country table

--- dom.js
+++ dom.js
@@ -12,21 +12,19 @@ const countries = [
   },
 ];
 
-
 // for (const country of countries) {
 //   aSampleString += "<li>Hiiiiii !!!</li>";
 // }
 
-for(const country of countries){
-    // aSampleString += "<li>${country.name}</li>";
-    aSampleString += "<li>" + country.name + "</li>";
-  }
+for (const country of countries) {
+  // aSampleString += "<li>${country.name}</li>";
+  aSampleString += "<li>" + country.name + "</li>";
+}
 
-for(const country of countries){
-    // aSampleString += "<li>${country.name}</li>";
-    tdString += `<tr><td> ${country.name} </td><td> ${country.code} </td></tr>`;
-  }
+for (const country of countries) {
+  aSampleString += "<li>" + country.name + "</li>";
+  // tdString += `<tr><td> ${country.name} </td><td> ${country.code} </td></tr>`;
+}
 
-  
 document.getElementById("myContainer").innerHTML = aSampleString;
 document.getElementById("myTable").innerHTML = tdString;
---
+++ domtable.js
@@ -0,0 +1,65 @@
+let myTable = "";
+
+// const countries = [
+//   { cnname: "Country name",
+//   mohadesh: "reagion",
+//   reagion:"Asia",
+//    name: "Bangladesh",
+//     code: "Conuntry code",
+//     cncode:"Bd"
+//   },
+//   {
+//     cnname: "Country name",
+//     mohadesh:"reagion",
+//     reagion:"Asia",
+//     name: "India",
+//     code: "Country Code",
+//     cncode:"In"
+//   },
+//   {
+//     cnname: "Country name",
+//     mohadesh:"reagion",
+//     reagion:"us state",
+//     name: "Canada",
+//     code: "Country Code",
+//     cncode:"ca"
+//   },
+//   {
+//     cnname: "Country name",
+//     mohadesh:"reagion",
+//     reagion:"us state",
+//     name: "America",
+//     code: "Country Code",
+//     cncode:"us"
+//   },
+// ];
+const countries = [
+  { reagion: "Asia", name: "Bangladesh", cncode: "Bd" },
+  {
+    reagion: "Asia",
+    name: "India",
+    cncode: "In",
+  },
+  {
+    reagion: "Asia",
+    name: "Canada",
+    cncode: "ca",
+  },
+  {
+    reagion: "Asia",
+    name: "America",
+    cncode: "us",
+  },
+];
+let tableHead = `<tr><th>Country Name</th><th>Region</th><th>Country Code</th></tr>`
+for (const country of countries) {
+  myTable +=
+    "<tr><td>" +
+    country.name +
+    "</td><td>" +
+    country.reagion +
+    "</td><td>" +
+    country.cncode +
+    "</td></tr>";
+}
+document.getElementById("myTable").innerHTML = tableHead + myTable;
