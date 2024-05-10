83ca26f1e7a96a71352174b1cd1afd6618163ebf
Rakim Ali <rakimali770@gmail.com>
2024-05-11 02:16:53 +0600

    code nothing work

--- photogallary/app.js
+++ photogallary/app.js
@@ -4,9 +4,26 @@ fetch(url)
   .then((datax) => datax.json())
   .then((data) => {
     for (const x of data) {
-      list += `<img src="${x.download_url}" width="250px">`;
+      list += `<img src="${x.download_url}" width="400px">`;
     }
     const tbodyOfTable = document.querySelector("#fetchGallery");
     tbodyOfTable.innerHTML = list;
   })
   .catch(error => console.error('Error:', error));
+
+
+
+  /*
+  let list = "";
+let url = "https://picsum.photos/v2/list?page=2&limit=100";
+fetch(url)
+  .then((datax) => start.json())
+  .then((data) => {
+    for (const x of data) {
+      list += `<img src= ${x.download_url} width ="250px">`;
+    }
+    const tbodyOfTable = document.querySelector("#fetchGallery");
+    tbodyOfTable.innerHTML = list;
+  });
+  `.catch`(error => console.error('Error:',error)); 
+  */
---
+++ weather/weather.js
@@ -0,0 +1,16 @@
+let list = "";
+let weatherUrl =
+  "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
+
+fetch(weatherUrl)
+  .then((x) => x.json())
+  .then((x) => {
+    for (const y of hourly) { // Assuming you want to iterate over the hourly forecast
+      list += `<p>${y.hourly}</p>`;
+    //   list += `<p>${y.relative_humidity_2m}</p>`;
+    //   list += `<p>${y.wind_speed_10m}</p>`;
+    }
+    const weather = document.querySelector("#weather");
+    weather.innerHTML = list;
+  })
+  .catch((error) => console.error('Error:', error));
