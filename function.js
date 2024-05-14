91b7af15030acf6307faf29ef4952c39594a02d3
Rakim Ali <rakimali770@gmail.com>
2024-05-14 18:08:59 +0600

    learn js function arrow

---
+++ function.js
@@ -0,0 +1,31 @@
+// standard function definition
+
+function sayHello1(){
+	return "Hello1";
+}
+
+// anonymous
+
+let sayHello2 = function(){
+	return "Hello2";
+}
+
+
+// arrow function
+let sayHello3 = () => {
+	return "Hello mian zadid sir";
+}
+
+console.log(sayHello3());
+
+let sayHello4 = () => { return "Hello Rakim Bhai";}
+
+console.log(sayHello4());
+
+
+
+	
+let greet = ( aGreeting ) => aGreeting
+
+
+console.log( greet("assalamu alaikum") );
