b75231c80e1c26699b20ce1a37842655bd6ef448
Rakim Ali <rakimali770@gmail.com>
2024-05-18 01:24:04 +0600

    learning  not equal

--- if-else.js
+++ if-else.js
@@ -1,6 +1,12 @@
 const emilregestration = "rakimali770@gmail.com";
 const emil = "rakimali770@gmail.com";
 
-if( emilregestration ===emil){ //checking input validations
-    console.log('everything is okh');
+if (emilregestration === emil) {
+  //checking input validations
+  console.log("everything is okh");      //if validation
 }
+
+
+let apple = "Apple";
+let orange = "Orange";
+console.log("Apple" != "Orange"); //  not equal
