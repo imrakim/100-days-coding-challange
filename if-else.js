6e5c257cfa9c4069ab4515db9c7af5e486fccf3b
Rakim Ali <rakimali770@gmail.com>
2024-05-19 14:57:54 +0600

    learning if else

--- if-else.js
+++ if-else.js
@@ -1,19 +1,23 @@
-const emilregestration = "rakimali770@gmail.com";
-const emil = "rakimali770@gmail.com";
+const loginPassword = "rakim123@";
+const loginEmail = "rakimali770@gmail.com";
 
-if (emilregestration === emil) {
-  //checking input validations
-  console.log("everything is okh");      //if validation
+const registerPassword = "rakim123@";
+const registerEmail = "rakimali770@gmail.com";
+
+
+
+if(loginPassword === registerPassword) {
+  console.log('Login success');
 }
+else{
+  console.log('enter correct Password');
+}
+// let apple = "Apple";
+// let orange = "Orange";
+// console.log("Apple" != "Orange"); //  not equal
 
+// let number = 8;
+// let string = 8;
 
-let apple = "Apple";
-let orange = "Orange";
-console.log("Apple" != "Orange"); //  not equal
-
-
-
-let number ='7';
-let string = '7';
-
-console.log(number !== string);  // Output: false
+// console.log(number !== string);
+// Output: false // when data type and value is Diffrent then the console show True - when datatype and value is same  then console show you false.
