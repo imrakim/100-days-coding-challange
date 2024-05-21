d6a69db4a504aeeed5b5677e4e0503b6d3b30ee2
Rakim Ali <rakimali770@gmail.com>
2024-05-21 15:31:55 +0600

    alert

--- dom.js
+++ dom.js
@@ -1,23 +1,29 @@
-let aSampleString = "";
-let myTable = "";
+// let aSampleString = "";
+// let myTable = "";
 
-const countries = [
-  {
-    name: "Bangladesh",
-    code: "BD",
-  },
-  {
-    name: "India",
-    code: "IN",
-  },
-];
+// const countries = [
+//   {
+//     name: "Bangladesh",
+//     code: "BD",
+//   },
+//   {
+//     name: "India",
+//     code: "IN",
+//   },
+// ];
 
-for (const country of countries) {
-  aSampleString += `<li> ${country.name}</li>`;
+// for (const country of countries) {
+//   aSampleString += `<li> ${country.name}</li>`;
+// }
+// for (const country of countries) {
+//   myTable += `<tr><td> ${country.name}</td></tr>`;
+// }
+
+// document.getElementById("myContainer").innerHTML = aSampleString;
+// document.getElementById("myTable").innerHTML = myTable;
+
+
+
+function rakim(){
+  alert("Im Rakim");
 }
-for (const country of countries) {
-  myTable += `<tr><td> ${country.name}</td></tr>`;
-}
-
-document.getElementById("myContainer").innerHTML = aSampleString;
-document.getElementById("myTable").innerHTML = myTable;
