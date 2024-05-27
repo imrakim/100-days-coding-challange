1dfeb858ccaf8ae0f0071c4ed3c18858184427f5
Rakim Ali <rakimali770@gmail.com>
2024-05-27 11:22:12 +0600

    initial arrary of object

---
+++ arrofobj.js
@@ -0,0 +1,28 @@
+let obj = [
+    {
+        name: "Rakim",
+        age : 21,
+        class: 10,
+        "school name": "Sirajuddin Sarker viddyniketan and collage"
+    },
+    {
+        name: "Rahim",
+        age : 20,
+        class: 10,
+        "school name": "Sirajuddin Sarker viddyniketan and collage"
+    },
+    {
+        name: "Rafi",
+        age : 22,
+        class: 10,
+        "school name": "Sirajuddin Sarker viddyniketan and collage"
+    },
+    {
+        name: "Ratul",
+        age : 23,
+        class: 10,
+        "school name": "Sirajuddin Sarker viddyniketan and collage"
+    }
+];
+
+console.log(Object.values(obj[0]));
