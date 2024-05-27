2104b8e1e1e9ed67b13ebdc583e72cee2bde494b
Rakim Ali <rakimali770@gmail.com>
2024-05-27 20:39:10 +0600

    initially commit

--- arrofobj.js
+++ arrofobj.js
@@ -1,47 +1,119 @@
-let products = [
+// let products = [
+//   {
+//     id: "rakim0",
+//     name: "Rakim",
+//     age: 21,
+//     class: 10,
+//     "school name": "Sirajuddin Sarker viddyniketan and collage",
+//   },
+//   {
+//     id: "rahim1",
+//     name: "Rahim",
+//     age: 20,
+//     class: 10,
+//     "school name": "Sirajuddin Sarker viddyniketan and collage",
+//   },
+//   {
+//     id: "rafi2",
+//     name: "Rafi",
+//     age: 22,
+//     class: 10,
+//     "school name": "Sirajuddin Sarker viddyniketan and collage",
+//   },
+//   {
+//     id: "ratul3",
+//     name: "Ratul",
+//     age: 23,
+//     class: 10,
+//     "school name": "Sirajuddin Sarker viddyniketan and collage",
+//     list: [
+//       {
+//         name: "rakim",
+//         roll: 12,
+//         class: 11,
+//         "collage name": "Uttara High School and college",
+//       },
+//     ],
+//   },
+// ];
+
+// const search = "ratul3";
+
+// for (let i = 0; i < products.length; i++) {
+//   let product = products[i];
+//   if (product.id === search) {
+//     console.log(product);
+//   }
+// }
+
+let newProduct = [
   {
-    id: "rakim0",
-    name: "Rakim",
-    age: 21,
-    class: 10,
-    "school name": "Sirajuddin Sarker viddyniketan and collage",
+    id: "product0",
+    name: "itel",
+    model: "it21711",
+    color: "blue",
+    stock: "available",
+    "with eqipment": ["charger", "earphone", "bacic operates note", "pappers"],
+    price: "1200 BDT",
   },
   {
-    id: "rahim1",
-    name: "Rahim",
-    age: 20,
-    class: 10,
-    "school name": "Sirajuddin Sarker viddyniketan and collage",
+    id: "product1",
+    name: "itel",
+    model: "it21711",
+    color: "blue",
+    stock: "available",
+    "with eqipment": ["charger", "earphone", "bacic operates note", "pappers"],
+    price: "1200 BDT",
   },
   {
-    id: "rafi2",
-    name: "Rafi",
-    age: 22,
-    class: 10,
-    "school name": "Sirajuddin Sarker viddyniketan and collage",
+    id: "product2",
+    name: "itel",
+    model: "it21711",
+    color: "blue",
+    stock: "available",
+    "with eqipment": ["charger", "earphone", "bacic operates note", "pappers"],
+    price: "1200 BDT",
   },
   {
-    id: "ratul3",
-    name: "Ratul",
-    age: 23,
-    class: 10,
-    "school name": "Sirajuddin Sarker viddyniketan and collage",
-    list: [
-      {
-        name: "rakim",
-        roll: 12,
-        class: 11,
-        "collage name": "Uttara High School and college",
-      },
-    ],
+    id: "product3",
+    name: "itel",
+    model: "it21711",
+    color: "blue",
+    stock: "available",
+    "with eqipment": ["charger", "earphone", "bacic operates note", "pappers"],
+    price: "1200 BDT",
+  },
+  {
+    id: "product4",
+    name: "itel",
+    model: "it21711",
+    color: "blue",
+    stock: "available",
+    "with eqipment": ["charger", "earphone", "bacic operates note", "pappers"],
+    price: "1200 BDT",
+  },
+  {
+    id: "product5",
+    name: "itel",
+    model: "it21711",
+    color: "blue",
+    stock: "available",
+    "with eqipment": ["charger", "earphone", "bacic operates note", "pappers"],
+    price: "1200 BDT",
   },
 ];
 
-const search = "ratul3";
-
-for (let i = 0; i < products.length; i++) {
-  let product = products[i];
-  if (product.id === search) {
-    console.log(product);
+let mobileId = "product5";
+for (let i = 0; i < newProduct.length; i++) {
+  let products = newProduct[i];
+  if (products.id === mobileId) {
+    console.log(products);
   }
 }
+// let mobileId = "product5";
+// for (let i = 0; i < newProduct.length; i++) {
+//   let products = newProduct[i];
+//   if (products.id === mobileId) {
+//     console.log(products);
+//   }
+// }
