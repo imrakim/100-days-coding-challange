8274b45ef80edd9599e34fad6d8e1d529057a503
Rakim Ali <rakimali770@gmail.com>
2024-05-27 12:54:16 +0600

    there i make when user search any product then the code will show search product deatails

--- arrofobj.js
+++ arrofobj.js
@@ -1,36 +1,47 @@
-let obj = [
-    {
-        name: "Rakim",
-        age : 21,
-        class: 10,
-        "school name": "Sirajuddin Sarker viddyniketan and collage"
-    },
-    {
-        name: "Rahim",
-        age : 20,
-        class: 10,
-        "school name": "Sirajuddin Sarker viddyniketan and collage"
-    },
-    {
-        name: "Rafi",
-        age : 22,
-        class: 10,
-        "school name": "Sirajuddin Sarker viddyniketan and collage"
-    },
-    {
-        name: "Ratul",
-        age : 23,
-        class: 10,
-        "school name": "Sirajuddin Sarker viddyniketan and collage",
-        list:[
-            {
-                name: 'rakim',
-                roll: 12,
-                class:11,
-                "collage name":'Uttara High School and college'
-            }
-        ]
-    }
+let products = [
+  {
+    id: "rakim0",
+    name: "Rakim",
+    age: 21,
+    class: 10,
+    "school name": "Sirajuddin Sarker viddyniketan and collage",
+  },
+  {
+    id: "rahim1",
+    name: "Rahim",
+    age: 20,
+    class: 10,
+    "school name": "Sirajuddin Sarker viddyniketan and collage",
+  },
+  {
+    id: "rafi2",
+    name: "Rafi",
+    age: 22,
+    class: 10,
+    "school name": "Sirajuddin Sarker viddyniketan and collage",
+  },
+  {
+    id: "ratul3",
+    name: "Ratul",
+    age: 23,
+    class: 10,
+    "school name": "Sirajuddin Sarker viddyniketan and collage",
+    list: [
+      {
+        name: "rakim",
+        roll: 12,
+        class: 11,
+        "collage name": "Uttara High School and college",
+      },
+    ],
+  },
 ];
 
-console.log(obj[3].list[0].name);
+const search = "ratul3";
+
+for (let i = 0; i < products.length; i++) {
+  let product = products[i];
+  if (product.id === search) {
+    console.log(product);
+  }
+}
