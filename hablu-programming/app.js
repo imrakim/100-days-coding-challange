e04ad484834eac6fe994eb25ab58c2eaad32d7d9
Rakim Ali <rakimali770@gmail.com>
2024-06-09 11:55:57 +0600

    bootstrap alert

--- hablu-programming/app.js
+++ hablu-programming/app.js
@@ -1,2 +1,31 @@
-let upperCase = 'Im Rakim';
-let text = upperCase.toUpperCase();
+// window.onload = function(){
+//     document.getElementById('close').onclick = function(){
+//         this.parentNode.parentNode.remove();
+//         return false;
+//     };
+// };
+
+// function clos(){
+// let x = document.getElementById('close');
+// x.remove()
+// }
+
+// script.js
+// script.js
+// script.js
+
+// script.js
+
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const alertDiv = document.querySelector('.alert');
+    if (alertDiv) {
+        alertDiv.querySelector('.close').addEventListener('click', () => closeAlert(alertDiv));
+    }
+});
+
+function closeAlert(alertDiv) {
+    alertDiv.style.opacity = '0';
+    setTimeout(() => alertDiv.remove(), 600);
+}
