d7469b9e4a3be9e76ef9ccf71f392e0a96989ef0
rakim <rakimali770@gmail.com>
2024-06-19 01:28:39 +0600

    learning string methods in js dom

---
+++ string-method/string.js
@@ -0,0 +1,14 @@
+const str = " I'm Rakim Ali"
+let check = str.length;
+let char =  str.charCodeAt(0); 
+console.log(char);
+
+
+let ra = "abcdef";
+const rss = ra.charAt(5);
+console.log(rss);
+
+
+let wht = "Im Rakim Ali";
+const ttt =  wht.substring(3,8); // slice() sunstring()  same methods in js.
+console.log(ttt);
