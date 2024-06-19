2687d7f9821ea3f8b6e1e38e7636b8039b4a9990
rakim <rakimali770@gmail.com>
2024-06-19 12:28:20 +0600

    learn string

--- string-method/string.js
+++ string-method/string.js
@@ -1,14 +1,21 @@
-const str = " I'm Rakim Ali"
+const str = " I'm Rakim Ali";
 let check = str.length;
-let char =  str.charCodeAt(0); 
+let char = str.charCodeAt(0);
 console.log(char);
-
 
 let ra = "abcdef";
 const rss = ra.charAt(5);
 console.log(rss);
 
-
 let wht = "Im Rakim Ali";
-const ttt =  wht.substring(3,8); // slice() sunstring()  same methods in js.
+const ttt = wht.substring(3, 8); // slice() sunstring()  same methods in js.
 console.log(ttt);
+
+
+let sta = "Im Rakim Ali";
+const star = sta.trimEnd();  // frist to last index string or number anything
+console.log(star);
+
+let num = "7";
+let formattedNum = num.padStart(2, '0'); // "007"  there nothing i understand.
+console.log(formattedNum);
